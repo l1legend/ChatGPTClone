9cd5d27e0a6397d8a015094485473186e2d70563
minTwin <michaelmin2020@gmail.com>
2023-09-15 17:14:01 -0700

    implement message state and onSubmit handler

--- chatty-pete-course/pages/chat/[[...chatId]].js
+++ chatty-pete-course/pages/chat/[[...chatId]].js
@@ -1,7 +1,15 @@
 import { ChatSidebar } from "components/ChatSidebar";
 import Head from "next/head";
+import { useState } from "react";
 
 export default function ChatPage() {
+  const [messageText, setMessageText] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("MESSAGE TEXT: ", messageText);    
+  };
+
   return (
     <>
       <Head>
@@ -12,9 +20,11 @@ export default function ChatPage() {
         <div className="flex flex-col bg-gray-700">
           <div className="flex-1">Chat window</div>
           <footer className="bg-gray-800 p-10">
-            <form>
+            <form onSubmit={handleSubmit}>
               <fieldset className="flex gap-2">
                 <textarea
+                  value={messageText}
+                  onChange={(e) => setMessageText(e.target.value)}
                   placeholder="Send a message to your AI friend..."
                   className="w-full resize-none rounded-md bg-gray-700 p-2 text-white
                   focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500"
