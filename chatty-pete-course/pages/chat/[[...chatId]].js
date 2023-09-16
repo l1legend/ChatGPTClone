22d3c9f07419cddb762bc0086996637b3c53d00d
minTwin <michaelmin2020@gmail.com>
2023-09-15 17:03:56 -0700

    added styling to textarea

--- chatty-pete-course/pages/chat/[[...chatId]].js
+++ chatty-pete-course/pages/chat/[[...chatId]].js
@@ -11,7 +11,17 @@ export default function ChatPage() {
         <ChatSidebar />
         <div className="flex flex-col bg-gray-700">
           <div className="flex-1">Chat window</div>
-          <footer className="bg-gray-800 p-10">footer</footer>
+          <footer className="bg-gray-800 p-10">
+            <form>
+              <fieldset className="flex gap-2">
+                <textarea
+                  className="w-full resize-none rounded-md bg-gray-700 p-2 text-white
+                  focus:border-emerald-500 focus:bg-gray-600 focus:ring focus:ring-emerald-500"
+                />
+                <button type="submit">Send</button>
+              </fieldset>
+            </form>
+          </footer>
         </div>
       </div>
     </>
